06cca6919537cc5a627c23f760d799201be34706
Franco Biasin <franconbiasin@gmail.com>
2020-10-26 21:05:37 -0300

    Rename java package to io.java.github.frankitox.native_exception

--- android/src/main/java/io/github/frankitox/native_exception/NativeExceptionModule.java
+++ android/src/main/java/io/github/frankitox/native_exception/NativeExceptionModule.java
@@ -1,4 +1,4 @@
-package com.reactlibrary;
+package io.github.frankitox.native_exception;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
--- android/src/main/java/io/github/frankitox/native_exception/NativeExceptionPackage.java
+++ android/src/main/java/io/github/frankitox/native_exception/NativeExceptionPackage.java
@@ -1,4 +1,4 @@
-package com.reactlibrary;
+package io.github.frankitox.native_exception;
 
 import java.util.Arrays;
 import java.util.Collections;
